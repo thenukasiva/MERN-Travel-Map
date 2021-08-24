83a70939b2df37516b9a06f04ac75e825bc24832
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2021-08-23 21:14:18 -0400

    connecting to db

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const app = express();
+
+dotenv.config();
+
+app.use(express.json())
+mongoose
+    .connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true } )
+    .then(() => {
+    console.log("MongoDB Connected!");
+})
+.catch((err) => console.log(err));
+
+app.listen(8800, () => {
+    console.log("Backend server is running!")
+})

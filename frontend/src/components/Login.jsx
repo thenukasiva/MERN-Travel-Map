a90714222118f2f6c4b0ee3ea95465d61dfc4049
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2021-09-11 16:57:39 -0400

    remove login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -28,7 +28,7 @@ export default function Login({ setShowLogin, setCurrentUsername,myStorage }) {
     <div className="loginContainer">
       <div className="logo">
         <Room className="logoIcon" />
-        <span>LogIn</span>
+        <span></span>
       </div>
       <form onSubmit={handleSubmit}>
         <input autoFocus placeholder="username" ref={usernameRef} />
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -30,7 +30,7 @@ export default function Register({ setShowRegister }) {
     <div className="registerContainer">
       <div className="logo">
         <Room className="logoIcon" />
-        <span>LogIn</span>
+        <span></span>
       </div>
       <form onSubmit={handleSubmit}>
         <input autoFocus placeholder="username" ref={usernameRef} />
